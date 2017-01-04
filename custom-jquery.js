9cd8fc9b5befbbbfaeabd53c13af883ad0aff93b
Jonathan Popenuck <john.popenuck@gmail.com>
2017-01-04 10:37:44 -0800

    started working on val

--- custom-jquery.js
+++ custom-jquery.js
@@ -57,19 +57,51 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
   jQueryObject.prototype.hasClass = function(className) {
+    var hasClass = false;
     this.each(function(element){
-      return element.classList.includes(className);
+      hasClass = hasClass || element.classList.contains(className);
     });
+    return hasClass;
+  };
+
+  jQueryObject.prototype.addClass = function(className) {
+    this.each(function(element){
+      element.classList.add(className);
+    });
+    return this.selection;
+  };
+
+  jQueryObject.prototype.removeClass = function(className) {
+    this.each(function(element){
+      element.classList.remove(className);
+    });
+    return this.selection;
+  };
+
+  jQueryObject.prototype.toggleClass = function(className) {
+    this.each(function(element){
+      element.classList.toggle(className);
+    });
+    return this.selection;
+  };
+
+  jQueryObject.prototype.val = function(value) {
+    if (value) {
+      this.each(function(element){
+        element.classList.remove(className);
+      });
+      return this.selection;
+    } else {
+      return this.idx(0);
+    }
   };
 
   jQueryObject.prototype.each = function(funktion) {
 
-    console.log(this);
-
     var selection = this.selection;
 
     for (var i = 0; i < selection.length; i++) {
-      funktion([i], i, selection);
+      funktion(selection[i], i, selection);
     }
 
   };
