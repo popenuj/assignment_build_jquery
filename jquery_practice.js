66f8f6759203475f9bf65cdbdb8fd23f3365ef59
Jonathan Popenuck <john.popenuck@gmail.com>
2017-01-04 08:18:52 -0800

    through each for function

---
+++ jquery_practice.js
@@ -0,0 +1,25 @@
+function Foo(property) {
+  this.property = property;
+}
+Foo.prototype.sampleMethod = function() {
+  console.log("method")
+};
+
+var Bar = function(property) {
+  return {};
+};
+
+function Baz(property) {
+  if (!(this instanceof Baz)) return new Baz(property);
+  this.property = property;
+}
+
+function SimpleObject() {
+  this.collection = [1,"foo",3];
+}
+SimpleObject.prototype.each = function(funktion) {
+  var coll = this.collection
+  for (var i = 0; i < coll.length; i++) {
+    funktion(coll[i], i, coll);
+  };
+}
